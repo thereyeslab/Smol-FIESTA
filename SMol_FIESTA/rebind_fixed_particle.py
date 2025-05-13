d9479d96412109476cf489d070d817b562016188
Steven Xu <stevenxu1202@yahoo.com>
2025-05-13 02:45:29 -0400

    fixed-particle assign spots to cells, simulate spot coords

--- SMol_FIESTA/rebind_fixed_particle.py
+++ SMol_FIESTA/rebind_fixed_particle.py
@@ -6,8 +6,9 @@ Incorporating ComDet fixed particle detection results to rebinding analysis
 - Isolate and calculate rebinding characteristics with new classification
 
 Input:
-    {comdet_path}/*_Cell_*_spotsAll.csv
+    {comdet_path}/*_Results.csv (from comdet)
     {csv_path}/{output_folder_name}/rebind-strict-event.csv
+    {mask_path}/*.png
 
 Output:
     TODO: annotate when done
@@ -19,14 +20,14 @@ Parameters:
 
 import numpy as np
 import pandas as pd
+from skimage import io as imgio
 import time
 import os
+from natsort import natsorted
 import logging
-import csv
 import shutil
 import tomllib
 import argparse
-
 
 def main(config_path: str = None):
     if not config_path:
@@ -53,9 +54,121 @@ def main(config_path: str = None):
     # set default parameter when not specified
     if isinstance(allowed_spot_overlap, str): allowed_spot_overlap = 0.0
 
-    # TODO: essentially two-part script (1. assign particles to each cell; 2. assign rebind events to particles)
+    # file lists
+    comdet_files = natsorted(get_file_names_with_ext(comdet_path, '.csv'))
+    mask_files = natsorted(get_file_names_with_ext(mask_path, '.png'))
+    rebind_df = pd.read_csv(str(os.path.join(output_path, 'rebind-strict-event.csv')))
+    rebind_df = rebind_df.loc[:, ~rebind_df.columns.str.contains('^Unnamed')]
+
+    if not len(comdet_files) == len(mask_files):
+        raise ValueError('Different number of Masks and ComDet output, you may have an image without spot.')
+
+    for i in range(len(mask_files)):
+        # read files
+        print_log('Processing', os.path.split(mask_files[i])[1], '->', os.path.split(comdet_files[i])[1])
+        mask = np.swapaxes(imgio.imread(mask_files[i]), 0, 1)
+        n_cell = np.max(mask)
+        comdet = pd.read_csv(comdet_files[i], index_col=0)
+        rebind_events = rebind_df[rebind_df['Video #'] == (i+1)]
+
+        # parse and assign comdet spots to cell
+        fixed_spots = [comdet.iloc[i].to_dict() for i in range(len(comdet))]
+        spots_cell = {}
+        _ = 0 # counter for eliminated spots
+        for spot in fixed_spots:
+            spot_center = (spot['X_(px)'], spot['Y_(px)'])
+            spot_ncell = mask[int(spot_center[0]), int(spot_center[1])]
+            if spot_ncell == 0:
+                _ += 1
+                continue
+            spot_bounds = (spot['xMin'], spot['yMin'], spot['xMax'], spot['yMax'])
+            spot_area = spot['NArea']
+            spot_cords = simulate_spot_coordinates(spot_center, spot_area, spot_bounds)
+            if spot_ncell in spots_cell:
+                spots_cell[spot_ncell].append((spot_center, spot_area, spot_bounds, spot_cords))
+            else:
+                spots_cell[spot_ncell] = [(spot_center, spot_area, spot_bounds, spot_cords)]
+        print_log('\t: Number of rogue spots (spots not in cell mask):', _)
+
+        # filter spots and cells based on overlap in the same cell
+        _ = [] # pending eliminated cells
+        for cell in spots_cell:
+            if len(spots_cell[cell]) < 2:
+                continue
+            spots = spots_cell[cell]
+            for j in range(len(spots_cell[cell]) - 1):
+                for k in range(len(spots_cell[cell])):
+                    overlap = 0
+                    for cords in spots[j][3]:
+                        if np.any(np.all(cords == spots[k][3], axis=1)):
+                            overlap += 1
+                    if (float(overlap) / spots[j][1] > allowed_spot_overlap
+                        or float(overlap) / spots[k][1] > allowed_spot_overlap):
+                        _.append(cell)
+        for cell in _:
+            del spots_cell[cell]
+        print_log('\t: Number of cells eliminated because of spots overlap:', len(_))
+
+
 
     return
+
+
+'''
+================================================================================================================
+FIXED-SPOTS PROCESSING
+================================================================================================================
+'''
+
+def simulate_spot_coordinates(center: tuple, area: int, bounds: tuple, max_iterable:int = 128):
+    '''
+    Simulate coordinates for each comdet-detected spots, +x, +y, -x, -y direction order
+    :param center: (float) center of detection -> only use int parts (x, y)
+    :param area: (int) area of the spot
+    :param bounds: (tuple(int)) xMin, yMin, xMax, yMax
+    :param max_iterable: maximum number of iterable coordinates the code will use for generating direction steps
+    :return: (list(int)) list of coordinates for the simulated spot
+    '''
+
+    n_pix = area - 1
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    step = 0
+    pix = np.array(center).astype(int)
+    coords = [pix.copy()]
+    min_bounds = [bounds[0], bounds[1]]
+    max_bounds = [bounds[2], bounds[3]]
+
+    # generate direction indices for a spiral out expansion of coordinates
+    dirindex = []
+    for i in range(256):
+        if i < 2:
+            dirindex.append(i)
+            continue
+        i1 = int(i / 2.0) + 1
+        dirindex += [i % len(directions)] * i1
+
+    while n_pix > 0:
+        pix = pix + directions[dirindex[step]]
+        step += 1
+
+        if ((pix >= min_bounds) & (pix <= max_bounds)).all():
+            coords.append(pix.copy())
+            n_pix -= 1
+    return coords
+'''
+================================================================================================================
+FILE HANDLING
+================================================================================================================
+'''
+
+# get all files under path with extension
+def get_file_names_with_ext(path: str, ext: str):
+    flist = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if os.path.splitext(file)[1] == ext:
+                flist.append(str(os.path.join(root, file)))
+    return flist
 
 '''
 ================================================================================================================
